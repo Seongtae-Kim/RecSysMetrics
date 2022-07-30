3ef4937120fd0c2a77a544dc33d5961d9e7cff9a
Seongtae-Kim <chris.seongtae@gmail.com>
2022-07-30 23:12:00 +0900

    Modifying TruthConditionalMetric class code to accept class size vectors

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from metrics import NDCG, RMSE, TruthConditionalMetric
 
 if __name__ == "__main__":
+        y_true = (["Apple"] * 7) + (["Orange"] * 2) + (["Mango"] * 1)\
+                + (["Apple"] * 4)\
+                + (["Orange"] * 10)\
+                + (["Mango"] * 12)
 
-    y_true = (["Apple"] * 7) + (["Orange"] * 2) + (["Mango"] * 1)\
-             + (["Apple"] * 4)\
-             + (["Orange"] * 10)\
-             + (["Mango"] * 12)
-        
-    y_pred = (["Apple"] * 7) + (["Orange"] * 2) + (["Mango"] * 1)\
-            + (["Orange"] * 1) + (["Mango"] * 3)\
-            + (["Apple"] * 8) + (["Mango"] * 2)\
-            + (["Apple"] * 9) + (["Orange"] * 3)
-        
-    tc = TruthConditionalMetric()
-    tc.build(y_trues=[y_true], y_preds=[y_pred])
-    tc.get_metric(verbose=True)
+        y_pred = (["Apple"] * 7) + (["Orange"] * 2) + (["Mango"] * 1)\
+                + (["Orange"] * 1) + (["Mango"] * 3)\
+                + (["Apple"] * 8) + (["Mango"] * 2)\
+                + (["Apple"] * 9) + (["Orange"] * 3)
+
+
+        itemindex = sorted(list(set(y_true).union(y_pred)))
+        t_vecs, p_vecs = [], []
+        for t, p in zip(y_true, y_pred):
+                t_vec = [1 if itemindex[i]==t else 0 for i in range(len(itemindex))]
+                p_vec = [1 if itemindex[i]==p else 0 for i in range(len(itemindex))]
+                t_vecs.append(t_vec)
+                p_vecs.append(p_vec)
+
+        y_true, y_pred = t_vecs, p_vecs
+
+        tc = TruthConditionalMetric(itemindex=itemindex)
+        tc.build(y_trues=y_true, y_preds=y_pred, verbose=True)
+        # tc.get_metric(verbose=True)
+
+        pass
--- metrics.py
+++ metrics.py
@@ -23,6 +23,7 @@ class RecSysMetric:
             assert k <= min(len(y_pred), len(y_true))
         except AssertionError:
             raise Exception(f"the length of k must be equal or small than the the smallest length between y_pred and y_true (min_length={min(len(y_pred), len(y_true))}).")
+    
 
 
 class RMSE(RecSysMetric):
@@ -52,91 +53,108 @@ class NDCG(RecSysMetric):
 
 
 class TruthConditionalMetric(RecSysMetric):
-    def __init__(self):
+    def __init__(self, itemindex):
         super().__init__()
-        self.confusion_matrix = None
+        self.itemindex=itemindex    
 
-    def build(self, y_trues, y_preds, k=None, verbose=False):
+    def build(self, y_trues, y_preds, binary=True, k=None, verbose=False):
+        """
+        y_trues:
+            shape: (number of instances, number of classes)
+        y_preds:
+            shape: (number of instances, number of classes)
+        """
+
         self.assert_lengths_equal(y_trues, y_preds)
+        if binary and k is not None:
+            raise AssertionError("You must set binary arguments as FALSE when you set k")
         
-        self.confusion_matrix={}        
+        total_metrics, class_freq = self.get_basic_metrics(y_trues, y_preds, binary)
+        for class_name in total_metrics:
+            base_metric = total_metrics[class_name]
+            total_metrics[class_name] = self.get_advanced_metrics(base_metric)
+        global_metric = self.get_global_metric(total_metrics, class_freq)
+
+
+        if verbose:
+            print("Stats by class")
+            for class_name in total_metrics:
+                print("class:",class_name)
+                for stat_type in total_metrics[class_name]:
+                    print(f"  {stat_type}:", total_metrics[class_name][stat_type])
+                print()
+            print()
+            print("Global stats")
+            for stat_type in global_metric:
+                print(f"  {stat_type}:", global_metric[stat_type])
+
+                    
+
+
+    def get_global_metric(self, total_metrics, class_freq):
+        support_sum = sum(class_freq.values())
+        global_metric = {}
+        for class_name in total_metrics:
+            for stat_type in total_metrics[class_name]:
+                if stat_type in ["TP", "TN", "FP", "FN"]:
+                    global_metric[stat_type] = global_metric.get(stat_type, 0) + total_metrics[class_name][stat_type]
+                elif stat_type in ["Precision", "Recall"]:
+                    global_metric.setdefault("Average " + stat_type, []).append(total_metrics[class_name][stat_type])
+                elif stat_type in ["F1-Score"]: # For Weighted F1 Score using support values
+                    global_metric["Weighted-Average " + stat_type] = global_metric.get("Weighted-Average " + stat_type, 0.) + (total_metrics[class_name][stat_type] * class_freq[class_name] / support_sum)
+
+        
+        global_metric = self.get_advanced_metrics(global_metric)
+        global_metric["Average Precision"] = sum(global_metric["Average Precision"]) / len(global_metric["Average Precision"])
+        global_metric["Average Recall"] = sum(global_metric["Average Recall"]) / len(global_metric["Average Recall"])
+        global_metric["Micro-Average F1 Score"] = 2 * (global_metric["Precision"] * global_metric["Recall"]) / (global_metric["Precision"] + global_metric["Recall"])
+        global_metric["Macro-Average F1 Score"] = 2 * (global_metric["Average Precision"] * global_metric["Average Recall"]) / (global_metric["Average Precision"] + global_metric["Average Recall"])
+        
+        return global_metric
+
+
+    def get_advanced_metrics(self, base_metric):
+        notlisted=list(set(["TP", "TN", "FP", "FN"]).difference(set(base_metric.keys())))
+        if len(notlisted) != 0:
+            raise AssertionError(f"The following stats are not available: {notlisted}")
+
+        adv_metric = {
+            "Accuracy": (base_metric["TP"]+base_metric["TN"]) / (base_metric["TP"]+base_metric["TP"]+base_metric["TN"]+base_metric["FN"]),
+            "Precision":(base_metric["TP"])/(base_metric["TP"]+base_metric["FP"]),
+            "Recall":(base_metric["TP"])/(base_metric["TP"]+base_metric["FN"]),
+            "Sensitivity": (base_metric["TP"])/(base_metric["TP"]+base_metric["FN"]),
+            "Specificity": (base_metric["TN"])/(base_metric["TN"]+base_metric["FP"]),
+            "False Positive Rate": (base_metric["FP"])/(base_metric["FP"]+base_metric["TN"]),
+        }
+        adv_metric["Misclassification Rate"] = 1 - adv_metric["Accuracy"]
+        adv_metric["F1-Score"] = 2 * (adv_metric["Precision"] * adv_metric["Recall"] / (adv_metric["Precision"] + adv_metric["Recall"]))
+
+        return adv_metric | base_metric
+
+
+    def get_basic_metrics(self, y_trues, y_preds, binary=True):
+        total_metrics = {item : {'TP':0, 'TN':0, 'FP':0, 'FN':0} for item in self.itemindex}
+        class_freq = {item:0 for item in self.itemindex} # For Weighted F1-Score
         for y_true, y_pred in zip(y_trues, y_preds):
             self.assert_lengths_equal(y_true, y_pred)
-            
-            if k: # For Precision@K
-                self.assert_k(y_true, y_pred, k)
-                y_true, y_pred = y_true[:k], y_pred[:k]
+            for i in range(len(y_true)):
+                true_val, pred_val = y_true[i], y_pred[i]
+                class_name = self.itemindex[i]
 
-            for t, p in zip(y_true, y_pred):
-                # Actual
-                self.confusion_matrix.setdefault(t, {t:0, p:0})
-                self.confusion_matrix.setdefault(p, {p:0, t:0})
+                if binary:
+                    if true_val == 1:
+                        class_freq[class_name]+=1
 
-                # Predicted
-                self.confusion_matrix[t][p] = self.confusion_matrix[t].get(p, 0) + 1
+                    if true_val == pred_val and true_val == 1:
+                        total_metrics[class_name]["TP"]+=1
+                    elif true_val == pred_val and true_val == 0:
+                        total_metrics[class_name]["TN"]+=1
+                    elif true_val == 1 and pred_val == 0:
+                        total_metrics[class_name]["FP"]+=1
+                    elif true_val == 0 and pred_val == 1:
+                        total_metrics[class_name]["FN"]+=1
 
-        if verbose:
-            for t in self.confusion_matrix:
-                for p in self.confusion_matrix[t]:
-                    print(f"<TRUE={t}> <PRED={p}>", end=" ")
-                    v = self.confusion_matrix[t][p]
-                    print(v)
+                elif not binary:
+                    pass
 
-
-    def get_metric(self, verbose=False):
-        if not self.confusion_matrix:
-            raise Exception("You Must Build Confusion Matrix First!")
-
-        metric={}
-        for true in self.confusion_matrix:
-            metric.setdefault(true, {})
-
-            # TRUE-POSITIVE
-            metric[true]["TP"] = self.confusion_matrix[true][true] 
-
-            # TRUE-NEGATIVE
-            metric[true]["TN"] = sum([sum([self.confusion_matrix[actual][pred] for pred in self.confusion_matrix[actual] if pred != true]) for actual in self.confusion_matrix if actual != true])
-
-            # FALSE-NEGATIVE
-            metric[true]["FN"] = sum([sum([self.confusion_matrix[actual][pred] for pred in self.confusion_matrix[actual] if pred == true]) for actual in self.confusion_matrix if actual != true]) 
-
-            # FALSE-POSITIVE
-            metric[true]["FP"] = sum([sum([self.confusion_matrix[actual][pred] for pred in self.confusion_matrix[actual] if pred != true]) for actual in self.confusion_matrix if actual == true]) 
-            
-            metric[true]["Precision"] = metric[true]["TP"] / (metric[true]["TP"] + metric[true]["FP"])
-            metric[true]["Recall"] = metric[true]["TP"] / (metric[true]["TP"] + metric[true]["FN"])
-            metric[true]["FalsePositiveRate"] = metric[true]["FP"] / (metric[true]["FP"] + metric[true]["TN"])
-            metric[true]["F1-Score"] = 2 * metric[true]["Precision"] * metric[true]["Recall"] / (metric[true]["Precision"] + metric[true]["Recall"])
-
-        total_meta_name = "TOTAL_RATE"
-        metric[total_meta_name] = {}
-        for name in ["TP", "TN", "FN", "FP", "Precision", "Recall", "FalsePositiveRate"]:
-            metric[total_meta_name]["Average " + name] = sum([metric[cat_name][name] for cat_name in metric if cat_name != total_meta_name]) / (len(metric)-1)
-        
-        self.metric = metric
-        
-        # Micro F1
-        micro_tp = sum([metric[cat_name]["TP"] for cat_name in metric if cat_name != total_meta_name])
-        micro_fp = sum([metric[cat_name]["FP"] for cat_name in metric if cat_name != total_meta_name])
-        micro_fn = sum([metric[cat_name]["FN"] for cat_name in metric if cat_name != total_meta_name])
-        micro_precision = micro_tp / (micro_tp + micro_fp)
-        micro_recall = micro_tp / (micro_tp + micro_fn)
-        micro_f1 = 2 * micro_precision * micro_recall / (micro_precision + micro_recall)
-        metric[total_meta_name]["Micro F1"] = micro_f1
-
-        # Macro F1
-        metric[total_meta_name]["Macro F1"] = sum([metric[cat_name]["F1-Score"] for cat_name in metric if cat_name != total_meta_name]) / (len(metric)-1)
-        
-        # Weighted F1
-        total_numbers = {true:sum(self.confusion_matrix[true].values()) for true in self.confusion_matrix}
-        metric[total_meta_name]["Weighted F1"] = sum([metric[cat_name]["F1-Score"] * total_numbers[cat_name] for cat_name in metric if cat_name != total_meta_name]) / sum(total_numbers.values())
-
-        if verbose:
-            for true in metric:
-                print(f"=={true}==")
-                for confusion in metric[true]:
-                    print(f"\t{confusion}: {metric[true][confusion]}")
-
-        self.metric = metric
-        self.total_meta_name = total_meta_name
-
+        return total_metrics, class_freq
